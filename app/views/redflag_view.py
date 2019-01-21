b3099381c008aca0e548d684cbdb54679175fd07
RachealN <rechealmatsikoIT@gmail.com>
2019-01-21 19:12:17 +0300

    initial commit

---
+++ app/views/redflag_view.py
@@ -0,0 +1,5 @@
+from flask import Blueprint,request,json,jsonify
+
+
+
+redflag_blueprint = Blueprint("redflag_blueprint", __name__)
---
+++ app/views/users_view.py
@@ -0,0 +1,4 @@
+from flask import Blueprint,request,json,jsonify
+
+
+Auth_blueprint = Blueprint("Auth_blueprint", __name__)
