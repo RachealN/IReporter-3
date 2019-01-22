c25be500b86388791e0e6e54b3d5705827a53d7c
RachealN <rechealmatsikoIT@gmail.com>
2019-01-22 03:26:58 +0300

    ft-login-user[Finishes #163377685]

--- app/controller/users_controller.py
+++ app/controller/users_controller.py
@@ -8,11 +8,16 @@ con = DatabaseConnection()
 class User:
     
     def register_user(self,firstname,lastname,email,password,phonenumber,username):
+        """This Function registers a user"""
+
         reg_user = "INSERT INTO users (firstname,lastname,email,password,phonenumber,username)\
                                          VALUES (%s,%s,%s,%s,%s,%s)"
         con.cursor.execute(reg_user, (firstname,lastname,email,password,phonenumber,username))
        
     def check_user_by_username(self,username):
+
+        """ Function to get_user_by_username"""
+
         check_for_unique_username = "SELECT id,username,isAdmin\
                                  FROM users WHERE username = %s"
         con.cursor.execute(check_for_unique_username, (username,))
@@ -20,8 +25,21 @@ class User:
         return row
 
     def check_user_by_email(self,email):
+
+        """ Function to get_user_by_email"""
+
         check_for_unique_email = "SELECT id,firstname,lastname,email,password,phonenumber,username\
                                  FROM users WHERE email = %s"
         con.cursor.execute(check_for_unique_email, (email,))
         row = con.cursor.fetchone()
         return row
+
+    def login(self,email):
+
+        """This function logins in a user"""
+
+        check_unique_email = "SELECT email WHERE username =%s"
+        con.cursor.execute(check_unique_email,(email))
+        row = con.cursor.fetchone()
+        return row
+
--- app/views/users_view.py
+++ app/views/users_view.py
@@ -1,17 +1,23 @@
 from flask import Blueprint,request,json,jsonify
 from app.validation import Validator
 from app.controller.users_controller import User
+from app.utilities.auth import AuthHelper
 
 
 Auth_blueprint = Blueprint("Auth_blueprint", __name__)
 
 register = Validator()
 user_db = User()
+token = AuthHelper()
+login = Validator()
+
 
 
 @Auth_blueprint.route('/auth/register', methods = ["POST"])
-
 def register_user():
+
+    """Handle POST request for this Endpoint. Url ---> /api/v1/auth/register"""
+
     request_data = request.get_json()
 
     firstname = request_data['firstname']
@@ -39,5 +45,31 @@ def register_user():
     user_db.check_user_by_email(email)
     return jsonify({'message':'user created successfully'}),201
 
-   
+
+
+@Auth_blueprint.route('/auth/login', methods = ["POST"])
+def login_user():
+
+    """Handle POST request for this Endpoint. Url ---> /api/vi/auth/login"""
+
+    request_data = request.get_json(force=True)
+    if len(request_data.keys()) != 2:
+        return jsonify({"message": "some fields are missing"}), 401
+    email = request_data['email']
+    password = request_data['password']
     
+    if not login.validate_email(email):
+        return jsonify({"message": "You entered an invalid email or email is missing"}), 401
+
+    if not login.validate_password(password):
+        return jsonify({"message": "You entered an invalid password or password should be atleast 8 characters long"}), 401
+
+
+    check_user = user_db.check_user_by_email(email)
+    if check_user:
+        payload = token.encode_auth_token(check_user)
+        return jsonify({
+            'message': 'login successful',
+            'auth_token': payload}), 200
+
+    return jsonify({"message": "You are not a system user"}), 401
